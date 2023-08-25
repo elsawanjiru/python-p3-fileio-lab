b82ee5d8aa73bd05fdbbe9570bd7593716ad426e
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-08-25 21:15:22 +0300

    completed assignment

--- lib/testing/file_io_test.py
+++ lib/testing/file_io_test.py
@@ -1,31 +1,31 @@
+import os
+import pytest
 from file_io import write_file, append_file, read_file
 
-def test_write_file(tmp_path):
-    """Test write_file()"""
-    file_name = tmp_path / "test_file"
+@pytest.fixture
+def temp_file_path(tmp_path):
+    return os.path.join(tmp_path, "test_file.txt")
+
+def test_write_file(temp_file_path):
     file_content = "This is a test content."
-    write_file(file_name, file_content)
-    with open(f'{file_name}.txt', 'r') as f:
-        file_content_read = f.read()
-    assert file_content_read == file_content
+    write_file(temp_file_path, file_content)
 
-def test_append_file(tmp_path):
-    """Test append_file()"""
+    with open(temp_file_path, 'r') as file:
+        assert file.read() == file_content
 
-    file_name = tmp_path / "test_file"
-    file_content = "This is a test content."
-    append_content = "\nAppended content."
-    write_file(file_name, file_content)
-    append_file(file_name, append_content)
-    with open(f'{file_name}.txt', 'r') as f:
-        file_content_read = f.read()
-    assert file_content_read == file_content + append_content
+def test_append_file(temp_file_path):
+    initial_content = "Initial content.\n"
+    append_file(temp_file_path, initial_content)
 
-def test_read_file(tmp_path):
-    """Test read_file()"""
+    appended_content = "This is appended content."
+    append_file(temp_file_path, appended_content)
 
-    file_name = tmp_path / "test_file"
-    file_content = "This is a test content."
-    write_file(file_name, file_content)
-    file_content_read = read_file(file_name)
-    assert file_content_read == file_content
+    with open(temp_file_path, 'r') as file:
+        assert file.read() == initial_content + appended_content
+
+def test_read_file(temp_file_path):
+    file_content = "This is the file content."
+    with open(temp_file_path, 'w') as file:
+        file.write(file_content)
+
+    assert read_file(temp_file_path) == file_content
